a3866afffae1491fa7b0e52ac9176507370e3748
Vaneor <vaneor88@gmail.com>
2024-09-12 18:30:44 -0500

    Abordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,17 @@
+"string" #string se utiliza para cadenas de texto
+'string' #asi tambien podemos generar un string
+"Hola mundo"
+'Hola mundo'
+
+"""Tus datos son:
+    nombre: Vanessa
+    apellido: Ortega"""
+'''Tus datos son:
+    nombre: Vanessa
+    apellido: Ortega'''
+#En Pyton tenemos diferentes tipos de números
+4 #número entero
+40.2#número tipo float
+#Tambien tenemos el tipo de datos boolean
+True #verdadero
+False #falso 
