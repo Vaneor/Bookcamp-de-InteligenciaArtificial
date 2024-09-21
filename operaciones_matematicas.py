f613c80728f4b0d26a2ca0e9a3209786d0b2ef26
Vaneor <vaneor88@gmail.com>
2024-09-21 10:51:17 -0500

    operaciones_matematicas.py

---
+++ operaciones_matematicas.py
@@ -0,0 +1,27 @@
+#Operadores matematicos
+a = 10
+b = 3
+print(a > b)
+print(b < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+print("Suma:", a + b)
+print("Reseta:", a - b)
+print("Multiplicacion:", a * b)
+print("poteenciacion:", a ** b)
+print("Division:", a / b)
+print("Modulo:", a % b)
+print("Division entera:", a // b)
+#queremos sumarle 2 a la variable a podrias poner a = a + 2 o mejor:
+a += 2
+print(a)
+
+operation_1 = 2 + 3 * 4
+operation_2 = (2 + 3) * 4
+print(operation_1)
+print(operation_2)
+operation_3 = (2 + 3) * (4 ** 2) / 8 - 1
+print(operation_3)
+
---
+++ operadores_de_entrada_y_salida.py
@@ -0,0 +1,5 @@
+nombre = input ("¿Cual es tu nombre?: ")
+print(nombre)
+print(type(nombre))
+edad = int(input ("¿Cual es tu edad?: "))
+print(type(edad)) #El resultado de input siempre sera string
